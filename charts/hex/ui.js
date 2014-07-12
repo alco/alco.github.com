307bcb23ddda2d516d9b5936378d1adaebbf23ff
Alexei Sholik <alcosholik@gmail.com>
2014-07-12 19:15:14 +0300

    Fix broken graphs

--- charts/hex/ui.js
+++ charts/hex/ui.js
@@ -1,22 +1,68 @@
 var dateFormat = d3.time.format("%Y-%m-%d");
 var parseDate = dateFormat.parse;
 
+var dateGetter = function(dict) { return dict.date; };
+var countGetter = function(dict) { return dict.count; };
+var sortByDate = function(data) { return _.sortBy(data, dateGetter); };
+
 var json = JSON.parse($("#json-data").html());
 var data = _.chain(json).map(function(list, key) {
-    return [key, _.map(list, function(dict) {
+    return [key, sortByDate(_.map(list, function(dict) {
         dict.date = parseDate(dict.date);
         return dict;
-    })];
+    }))];
 }).object().value();
-var flatData = _.flatten(_.map(data, function(list) {
-    return list;
-}));
+var flatData = _.chain(data)
+    .map(function(list) { return list; })
+    .flatten()
+    .value();
+flatData = sortByDate(flatData);
 
-var dates = _.flatten(_.map(data, function(list) {
-    return _.map(list, function(dict) { return dict.date; });
-}));
+var dateExtent = d3.extent(_.map(flatData, dateGetter));
 
-var dateExtent = d3.extent(dates);
+var weeklyConfig = {
+    interval: "week",
+    fromDate: moment(dateExtent[1]).subtract(1, "week").toDate(),
+    ticks: [d3.time.day, 1],
+    tickFormat: d3.time.format("%a %d"),
+};
+
+var monthlyConfig = {
+    interval: "month",
+    fromDate: moment(dateExtent[1]).subtract(1, "month").toDate(),
+};
+
+var yearlyConfig = {
+    interval: "year",
+    fromDate: moment(dateExtent[1]).subtract(1, "year").toDate(),
+};
+
+function getExtent(data, config, cummulative) {
+    var transform;
+    if (cummulative) {
+        transform = accumulateData;
+    } else {
+        transform = function(x) { return x; };
+    }
+    return d3.extent(_.chain(data).map(function(list) {
+        return _.map(transform(filterData(list, config.fromDate)), countGetter);
+    }).flatten().value());
+}
+
+var countExtents = {
+    week: {
+        true: getExtent(data, weeklyConfig, true),
+        false: getExtent(data, weeklyConfig, false),
+    },
+    month: {
+        true: getExtent(data, monthlyConfig, true),
+        false: getExtent(data, monthlyConfig, false),
+    },
+    year: {
+        true: getExtent(data, yearlyConfig, true),
+        false: getExtent(data, yearlyConfig, false),
+    },
+};
 
 var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 1000 - margin.left - margin.right,
@@ -38,20 +84,18 @@ var colorIndex = 0;
 
 /////////
 
-function newChart(dateExtent, data, config, invertY) {
-    var fromDate = moment(dateExtent[1]).subtract(1, config.interval).toDate();
-
-    var counts = _.chain(data)
-                    .filter(function(dict) { return dict.date >= fromDate; })
-                    .map(function(dict) { return dict.count; }).value();
-
+function newChart(dateExtent, inExtent, config, invertY, cummulative) {
     var x = d3.time.scale.utc()
-        .domain([Math.max(dateExtent[0], fromDate), dateExtent[1]])
+        .domain([Math.max(dateExtent[0], config.fromDate), dateExtent[1]])
         .range([0, width]);
     var xAccess = function(dict) { return x(dict.date); };
 
-    var extent = d3.extent(counts);
-    //extent[1] *= 10;
+    var extent;
+    if (inExtent) {
+        extent = inExtent;
+    } else {
+        extent = countExtents[config.interval][cummulative];
+    }
     var y = d3.scale.linear().domain(extent);
     y.range([height, 0]);
     var yAccess = function(dict) { return y(dict.count); };
@@ -101,7 +145,7 @@ function newChart(dateExtent, data, config, invertY) {
     return {
         svg: svg,
         chart: chart,
-        fromDate: fromDate,
+        fromDate: config.fromDate,
         useLine: invertY,
         x: xAccess,
         y: function(dict) { return y(dict.count); },
@@ -112,17 +156,7 @@ function filterData(data, fromDate) {
     return _.filter(data, function(dict) { return dict.date >= fromDate; });
 }
 
-function cummulativeData(data) {
-    var acc = 0;
-    return _.chain(data)
-        .sortBy(function(dict) { return dict.date; })
-        .map(function(dict) { acc += dict.count; var obj = {count: acc, date: dict.date}; return obj; })
-        .value();
-}
-
 function updateChart(chart, data, name) {
-    var data = filterData(data, chart.fromDate);
-    //var data = cummulativeData(data);
     var color = FLAT_COLORS[colorIndex++ % FLAT_COLORS.length];
     var w = Math.min(40, width / data.length);
     var wp = w * .7;
@@ -154,41 +188,28 @@ function updateChart(chart, data, name) {
 
 //////////////
 
-var weeklyConfig = {
-    interval: "week",
-    ticks: [d3.time.day, 1],
-    tickFormat: d3.time.format("%a %d"),
-};
-
-var monthlyConfig = {
-    interval: "month",
-};
-
-var yearlyConfig = {
-    interval: "year",
-};
-
 var currentPackage;
 var chartType = "bar";
 var charts = [];
 var totalChart;
 var cummulative = false;
 
-var totalData = _.reduce(data, function(acc, list) {
-    return _.reduce(list, function(acc, dict) {
-        if (!acc[dict.date]) {
-            acc[dict.date] = {date: dict.date, count: 0};
-        }
-        acc[dict.date].count += dict.count;
-        return acc;
-    }, acc);
-}, {});
-totalData = _.sortBy(totalData, function(dict) { return dict.date; });
+var totalData = _.map(_.reduce(flatData, function(acc, dict) {
+    if (!acc[dict.date]) {
+        acc[dict.date] = {date: dict.date, count: 0};
+    }
+    acc[dict.date].count += dict.count;
+    return acc;
+}, {}), function(dict) { return dict; });
+var totalAccumulatedData = accumulateData(totalData);
+var totalExtent = {
+    false: d3.extent(_.map(totalData, countGetter)),
+    true: d3.extent(_.map(totalAccumulatedData, countGetter)),
+};
 
 function accumulateData(data) {
     var count = 0;
     return _.chain(data)
-        .sortBy(function(dict) { return dict.date; })
         .map(function(dict) {
             count += dict.count;
             var obj = {};
@@ -204,19 +225,14 @@ function redrawCharts(type, package) {
 
     if (!package) return;
 
-    var fdata = flatData;
-    var pdata = data[package];
-    if (cummulative) {
-        var fdata = _.flatten(_.map(data, function(list) {
-            return accumulateData(list);
-        }));
-        pdata = accumulateData(pdata);
-    }
-
     var useLine = type == "line";
     charts = _.map([weeklyConfig, monthlyConfig, yearlyConfig], function(config) {
-        var chart = newChart(dateExtent, fdata, config, useLine);
-        return updateChart(chart, pdata, package);
+        var chart = newChart(dateExtent, null, config, useLine, cummulative);
+        var filteredData = filterData(data[package], chart.fromDate);
+        if (cummulative) {
+            filteredData = accumulateData(filteredData);
+        }
+        return updateChart(chart, filteredData, package);
     });
 }
 
@@ -231,11 +247,13 @@ function redrawTotalChart() {
     if (totalChart) {
         totalChart.remove();
     }
-    var data = totalData;
+    var data;
     if (cummulative) {
-        data = accumulateData(data);
+        data = totalAccumulatedData;
+    } else {
+        data = totalData;
     }
-    totalChart = newChart(dateExtent, data, yearlyConfig, chartType=="line");
+    totalChart = newChart(dateExtent, totalExtent[cummulative], yearlyConfig, chartType=="line", cummulative);
     totalChart = updateChart(totalChart, data, "total");
     totalChart.attr("class", "");
 }
